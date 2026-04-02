7f817a70578b3b6e7f75baaa527d8e65dde313a1
udi-collectors <DeviceCollectorsTeam@experian.com>
2026-04-02 06:20:28 -0500

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UDICollectorBinaryRemote",
-            url: "https://github.com/udi-collectors/udi-collector-ios/releases/download/1.2.4/udi-collector-ios-xcframework-1.2.4.zip", // Replace with actual URL during build
+            url: "https://github.com/udi-collectors/udi-collector-ios/releases/download/1.2.7/udi-collector-ios-xcframework-1.2.7.zip", // Replace with actual URL during build
             checksum: "5705fe4c985f718505e8f14b1fa8526a6db2876122ad9dae58db30d5ef14762e" // Replace with actual checksum during build
         )
     ]
