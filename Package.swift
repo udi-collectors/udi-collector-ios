ea6ffb55e363e86c14533b66d4889273ef665dda
udi-collectors <DeviceCollectorsTeam@experian.com>
2026-04-02 05:44:06 -0500

    Downgraded swift-tools-version
    Downgraded swift-tools-version to be compatible with Swifrt 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,9 @@ import PackageDescription
 let package = Package(
     name: "UDICollectorSDK",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UDICollectorSDK",
             targets: ["UDICollectorBinaryRemote"]
@@ -21,11 +20,5 @@ let package = Package(
             url: "https://github.com/udi-collectors/udi-collector-ios/releases/download/1.2.4/udi-collector-ios-xcframework-1.2.4.zip", // Replace with actual URL during build
             checksum: "5705fe4c985f718505e8f14b1fa8526a6db2876122ad9dae58db30d5ef14762e" // Replace with actual checksum during build
         )
-        // ,
-        // .binaryTarget(
-        //     name: "UDICollectorBinaryLocal",
-        //     path: "../sc-collector-sdk/target/frameworks/XCFramework/UDICollector.xcframework"
-        // )
-    ],
-    swiftLanguageModes: [.v5]
+    ]
 )
