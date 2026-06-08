50e648763a2d3024202c1157b16be3dbbc061e3c
Jenkins <jenkins@experian.com>
2026-06-08 08:11:25 +0000

    Release 9.4.0 - Updated XCFramework URL and checksum

--- Package.swift
+++ Package.swift
@@ -1,31 +1,24 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "UDICollectorSDK",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UDICollectorSDK",
-            targets: ["UDICollectorBinaryRemote"]
+            targets: ["UDICollectorSDK"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "UDICollectorBinaryRemote",
-            url: "https://github.com/udi-collectors/udi-collector-ios/releases/download/1.3.0/udi-collector-ios-xcframework-1.3.0.zip", // Replace with actual URL during build
-            checksum: "7adeb357f0f29be739ee6f88c0d231f5d27b420df7d3843661fce3456c8e4bd9" // Replace with actual checksum during build
+            name: "UDICollectorSDK",
+            url: "https://github.com/udi-collectors/udi-collector-ios/releases/download/9.4.0/udi-collector-ios-xcframework-9.4.0.zip", // Replace with actual URL during build
+            checksum: "e89bf832eccbde2cf9b56ccf595344ab5547cae009caa287b53456e15c022e56" // Replace with actual checksum during build
         )
-        // ,
-        // .binaryTarget(
-        //     name: "UDICollectorBinaryLocal",
-        //     path: "../sc-collector-sdk/target/frameworks/XCFramework/UDICollector.xcframework"
-        // )
-    ],
-    swiftLanguageModes: [.v5]
+    ]
 )
