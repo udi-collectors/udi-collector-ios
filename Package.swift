a1e50330dd1f51473d40415f35168d7e31de8b0f
Jenkins <jenkins@experian.com>
2026-08-27 08:50:17 +0000

    Release 9.4.0 - Updated XCFramework URL and checksum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UDICollectorSDK",
-            url: "https://github.com/udi-collectors/udi-collector-ios/releases/download/9.4.0/udi-collector-ios-xcframework-9.4.0.zip", // Replace with actual URL during build
+            url: "https://github.com/experian-collectors/deviceintelligence-ios/releases/download/9.4.0/deviceintelligence-collector-ios-xcframework-9.4.0.zip", // Replace with actual URL during build
             checksum: "e89bf832eccbde2cf9b56ccf595344ab5547cae009caa287b53456e15c022e56" // Replace with actual checksum during build
         )
     ]
